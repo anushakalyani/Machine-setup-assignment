9c6e2658432c38f8065a15e0f83dd61871390b05
anushakalyani <anusha.baroda@gmail.com>
2020-10-17 12:23:28 +0530

    Add sample test

---
+++ src/test/java/TestDemo.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class TestDemo {
+    @Test
+    void sampleTest() {
+        assertThat(1,is(equalTo(1)));
+    }
+}
